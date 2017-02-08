767d83d3aa6dc89ab444f284149fcefb31850708
CXO2 <com@cxo2.me>
2017-02-09 00:59:01 +0700

    Added template of missing pages
    Login, Signup and Profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 import { WritePage } from '../pages/write/write';
 import { SettingsPage } from '../pages/settings/settings';
 import { FeedPage } from '../pages/feed/feed';
+import { ProfilePage } from '../pages/profile/profile';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
+    LoginPage,
+    SignupPage,
     WritePage,
     SettingsPage,
     FeedPage,
+    ProfilePage,
     TabsPage
   ],
   imports: [
@@ -33,9 +39,12 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    LoginPage,
+    SignupPage,
     WritePage,
     SettingsPage,
     FeedPage,
+    ProfilePage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
