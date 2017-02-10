1ff55dda1d27716a637bd8cd65ee1bda6a8eb493
CXO2 <com@cxo2.me>
2017-02-10 14:55:35 +0700

    Updated Signup Page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { NavController, NavParams } from 'ionic-angular';
+
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-signup',
@@ -7,10 +11,21 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  fullname        : string;
+  email           : string;
+  password        : string;
+  confirmPassword : string;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  constructor(
+      private navCtrl  : NavController
+  ) {}
+
+  onRegister(registerForm : NgForm) {
+      // TODO: Register the user based on form input
+  }
+
+  goLogin() {
+    this.navCtrl.setRoot(LoginPage);
   }
 
 }
