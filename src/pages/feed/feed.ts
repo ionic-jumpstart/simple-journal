908e88761d63d08330004a7635dca3dc07227d8f
CXO2 <com@cxo2.me>
2017-02-12 21:23:14 +0700

    Updated FeedPage
    - Updated FeedPage layout design
    - FeedPage now show mock post data

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -8,8 +8,44 @@ import { NavController } from 'ionic-angular';
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController) {
+  private posts : any = [];
 
+  constructor(
+    public navCtrl: NavController
+  ) {
+  }
+
+  private ionViewDidLoad() {
+    this.loadPosts();
+  }
+
+  private loadPosts() {
+    this.posts = [
+      {
+        title   : 'A super interesting story',
+        time    : 'Febuary 20, 08:30 pm',
+        content : `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.\n
+        Requiescat in pace `
+      },
+      {
+        title   : 'Pada waktu itu',
+        time    : 'March 2, 08:30 pm',
+        content : `Pada suatu hari, ternyata, kisah tamat selesai`
+      },
+      {
+        title   : 'Auroram videre potest',
+        time    : 'January 01, 01:00 am',
+        content : 'Dormit deus faciunt dormit perpetua astra deus Auroram videre potest'
+      }
+    ];
+  }
+
+  private refresh(refresher : any) {
+    setTimeout(() => {
+      this.loadPosts();
+      refresher.complete();
+    }, 2000);
   }
 
 }
