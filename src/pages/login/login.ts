4a7ca1a1253274e20804f1191a2c0668b5a7ea6e
CXO2 <com@cxo2.me>
2017-02-12 21:21:17 +0700

    Added mock authentication process to LoginPage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { 
+  NavController, 
+  NavParams,
+  AlertController,
+  LoadingController 
+} from 'ionic-angular';
+
+import { UserData } from '../../providers/user-data';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
@@ -16,11 +23,45 @@ export class LoginPage {
   password : string;
 
   constructor(
-    private navCtrl  : NavController
+    private navCtrl     : NavController,
+    private alertCtrl   : AlertController,
+    private loadingCtrl : LoadingController,
+    private userData    : UserData
   ) {}
 
   onLogin(loginForm : NgForm) {
-    // TODO: Authenticate the user
+    // Perform authentication only if form is valid
+    if (loginForm.invalid){
+      return;
+    }
+
+    // Create loading dialog
+    // Mock the loading as well to 2 seconds
+    let loader = this.loadingCtrl.create({
+      content: 'Please wait..'
+    });
+
+    loader.present();
+    setTimeout(() => {
+      this.userData.authenticate(this.email, this.password).subscribe(
+        (data) => {
+          // Login Successful
+          console.info(data);
+          loader.dismiss();
+        },
+        (err) => {
+          // Login Failed, display error message
+          let alert = this.alertCtrl.create({
+            title   : 'Error',
+            message : err,
+            buttons : [ 'Close' ]
+          });
+
+          alert.present();
+          loader.dismiss();
+        }
+      );
+    }, 2000);
   }
 
   goSignup() {
